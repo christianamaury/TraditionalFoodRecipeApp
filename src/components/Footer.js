34d10638820c45bde0d8a64cc2c81d4d8f8e6be0
christianamaury <chris.amaury@hotmail.com>
2022-12-09 17:19:19 -0700

    Completion of the Privacy Agreement Page & Adding more CSS

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './footer.css';
+
+export const Footer = () => {
+  return (
+    <div className="FooterClass">
+    <p>
+      © Copyright Sweetest Interactive Ent.
+    </p>
+    </div>
+  )
+}
